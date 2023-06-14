a880d955d3e66c47386034330a941f34fa7de930
Finn <finn1999.duehrkop@gmail.com>
2023-06-14 18:08:47 +0200

    update tests to better reflect the intended behavior

--- Tests/ShoppingService.UnitTests/Test_ShoppingList_RemoveItem.cs
+++ Tests/ShoppingService.UnitTests/Test_ShoppingList_RemoveItem.cs
@@ -13,7 +13,7 @@ public sealed class Test_ShoppingList_RemoveItem
             ShoppingList.Conventions.NAME_MIN_LENGTH,
             ShoppingList.Conventions.NAME_MAX_LENGTH);
 
-    
+
     [Fact]
     public void Should_ReturnTrue_WhenProductIsIncludedInItems()
     {
@@ -24,22 +24,22 @@ public sealed class Test_ShoppingList_RemoveItem
 
         // Act
         bool removed = shoppingList.RemoveItem(productId);
-        
+
         // Assert
         using var scope = new AssertionScope();
         removed.Should().BeTrue("because the product {0} is included in the list", productId);
     }
-    
+
     [Fact]
     public void Should_ReturnFalse_WhenProductNotIncludedInItems()
     {
         // Arrange
-        Guid productId = Guid.NewGuid();
-        ShoppingList shoppingList = ShoppingList.CreateNew(_validShoppingListName);
+        var productId = Guid.NewGuid();
+        var shoppingList = ShoppingList.CreateNew(_validShoppingListName);
 
         // Act
         bool removed = shoppingList.RemoveItem(productId);
-        
+
         // Assert
         using var scope = new AssertionScope();
         removed.Should().BeFalse("because the product {0} is not included in the list", productId);
--- Tests/ShoppingService.UnitTests/Test_ShoppingList_UpdateItem.cs
+++ Tests/ShoppingService.UnitTests/Test_ShoppingList_UpdateItem.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using FluentAssertions.Execution;
 using ShoppingService.Domain;
+using ShoppingService.UnitTests.Assertions;
 
 namespace ShoppingService.UnitTests;
 
@@ -13,43 +14,42 @@ public sealed class Test_ShoppingList_UpdateItem
             ShoppingList.Conventions.NAME_MIN_LENGTH,
             ShoppingList.Conventions.NAME_MAX_LENGTH);
 
-    [Fact]
-    public void Should_ReturnTrue_WhenAmountIsUpdated()
+    [Theory]
+    [InlineData(1, 100), InlineData(100, 1)]
+    public void Should_ReturnTrue_WhenAmountIsUpdated(int initialAmount, int targetAmount)
     {
         // Arrange
-        Guid productId = Guid.NewGuid();
-        const int initValue = 1;
-        const int updatedValue = 100;
-        ShoppingList shoppingList = ShoppingList.CreateNew(_validShoppingListName);
-        shoppingList.AddItem(productId, initValue);
+        var shoppingList = ShoppingList.CreateNew(_validShoppingListName);
+
+        var productId = Guid.NewGuid();
+        shoppingList.AddItem(productId, initialAmount);
 
         // Act
-        bool updated = shoppingList.UpdateItem(productId, 100);
-        
+        bool updated = shoppingList.UpdateItem(productId, targetAmount);
+
         // Assert
         using var scope = new AssertionScope();
         updated.Should().BeTrue("because the amount of product {0} was updated", productId);
-        ShoppingListItem shoppingListItem = shoppingList.Items.Single(x => x.ProductId == productId);
-        shoppingListItem.Amount.Should().Be(updatedValue, "because the amount should be updated to {0}", updatedValue);
+        shoppingList.Items.Should().ContainSingle("because only a single item was added in setup")
+            .Which.Should().BeForProductId(productId, "because that is the product id of the item added during setup")
+            .And.HaveAmount(targetAmount, "because the update set the amount to {0}", targetAmount);
     }
-    
+
     [Fact]
-    public void Should_ReturnFalse_WhenAmountIsNotUpdated()
+    public void Should_ReturnFalse_WhenShoppingListDoesNotContainTheItem()
     {
         // Arrange
-        Guid productId = Guid.NewGuid();
-        const int initValue = 1;
-        const int updatedValue = 100;
-        ShoppingList shoppingList = ShoppingList.CreateNew(_validShoppingListName);
-        shoppingList.AddItem(productId, initValue);
+        var shoppingList = ShoppingList.CreateNew(_validShoppingListName);
+
+        Guid targetItemProductId = Guid.Parse("274D4C9D-12C0-4543-9996-CDDBBCF1C0D7");
+        const int targetAmount = 100;
 
         // Act
-        bool updated = shoppingList.UpdateItem(Guid.NewGuid(), 100);
-        
+        bool updated = shoppingList.UpdateItem(targetItemProductId, targetAmount);
+
         // Assert
         using var scope = new AssertionScope();
-        updated.Should().BeFalse("because the amount of product {0} was not updated", productId);
-        ShoppingListItem shoppingListItem = shoppingList.Items.Single(x => x.ProductId == productId);
-        shoppingListItem.Amount.Should().Be(initValue, "because the amount of {0} remains unchanged", initValue);
+        shoppingList.Items.Should().BeEmpty("because no items were added");
+        updated.Should().BeFalse("because the list does not contain an item with product id {0}", targetItemProductId);
     }
 }
