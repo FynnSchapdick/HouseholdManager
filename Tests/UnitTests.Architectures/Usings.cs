1f7f64a6dee420c4a0bb0adf6351baeb284d0837
Finn <finn1999.duehrkop@gmail.com>
2023-06-17 12:54:57 +0200

    Remove Artifacts

--- Tests/UnitTests.Architectures/Usings.cs
+++ Tests/UnitTests.Architectures/Usings.cs
@@ -3,5 +3,4 @@ global using ArchUnitNET.Loader;
 global using Xunit;
 global using ArchUnitNET.xUnit;
 
-//add a using directive to ArchUnitNET.Fluent.ArchRuleDefinition to easily define ArchRules
 global using static ArchUnitNET.Fluent.ArchRuleDefinition;
