3846f3300e3feda2d7addbdb5e31ba539c4feebf
FynnSchapdick <fschapdick@gmail.com>
2023-06-23 10:54:18 +0200

    Refactor versioning and simplify usage

--- Source/Backend/HouseholdManager.Api/Versioning/ConfigureSwaggerOptions.cs
+++ Source/Backend/HouseholdManager.Api/Versioning/ConfigureSwaggerOptions.cs
@@ -12,20 +12,20 @@ namespace HouseholdManager.Api.Versioning;
 /// </summary>
 /// <remarks>This allows API versioning to define a Swagger document per API version after the
 /// <see cref="IApiVersionDescriptionProvider"/> service has been resolved from the service container.</remarks>
-public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+public sealed class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
 {
-    private readonly IApiVersionDescriptionProvider provider;
+    private readonly IApiVersionDescriptionProvider _provider;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
     /// </summary>
     /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
-    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
 
     /// <inheritdoc />
     public void Configure(SwaggerGenOptions options)
     {
-        foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
+        foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
         {
             options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
         }
@@ -33,12 +33,19 @@ public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
 
     private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
     {
-        var text = new StringBuilder();
-        var info = new OpenApiInfo()
+        OpenApiInfo info = new OpenApiInfo
         {
             Title = "Household Manager API",
             Version = description.ApiVersion.ToString(),
+            Description = GetDescription(description)
         };
+
+        return info;
+    }
+
+    private static string GetDescription(ApiVersionDescription description)
+    {
+        StringBuilder text = new StringBuilder();
 
         if (description.IsDeprecated)
         {
@@ -47,35 +54,38 @@ public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
 
         if (description.SunsetPolicy is { } policy)
         {
-            if (policy.Date is { } when)
-            {
-                text.Append(" The API will be sunset on ")
-                    .Append(when.Date.ToShortDateString())
-                    .Append('.');
-            }
-
-            if (policy.HasLinks)
-            {
-                text.AppendLine();
-
-                foreach (LinkHeaderValue link in policy.Links)
-                {
-                    if (link.Type != "text/html") continue;
-
-                    text.AppendLine();
-
-                    if (link.Title.HasValue)
-                    {
-                        text.Append(link.Title.Value).Append(": ");
-                    }
-
-                    text.Append(link.LinkTarget.OriginalString);
-                }
-            }
+            AppendSunsetPolicyDetails(text, policy);
         }
 
-        info.Description = text.ToString();
+        return text.ToString();
+    }
+    
+    private static void AppendSunsetPolicyDetails(StringBuilder text, SunsetPolicy sunsetPolicy)
+    {
+        if (sunsetPolicy.Date is { } when)
+        {
+            text.Append(" The API will be sunset on ")
+                .Append(when.Date.ToShortDateString())
+                .Append('.');
+        }
 
-        return info;
+        if (!sunsetPolicy.HasLinks)
+        {
+            return;
+        }
+
+        text.AppendLine();
+
+        foreach (LinkHeaderValue link in sunsetPolicy.Links.Where(x => x.Type == "text/html"))
+        {
+            text.AppendLine();
+
+            if (link.Title.HasValue)
+            {
+                text.Append(link.Title.Value).Append(": ");
+            }
+
+            text.Append(link.LinkTarget.OriginalString);
+        }
     }
 }
